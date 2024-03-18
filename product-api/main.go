09e9e4e7c8c66130054850d2ac9bdc9051a1ab73
Aman <akapur07@gmail.com>
2024-03-18 20:46:46 +0530

    handlers integrated

--- product-api/main.go
+++ product-api/main.go
@@ -2,11 +2,29 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
+	"os"
+	"time"
+
+	"github.com/amankapur007/product-api/handlers"
 )
 
 func main() {
 	fmt.Println("product api")
+	l := log.New(os.Stdout, "products-api", log.LstdFlags)
+	productsHandler := handlers.NewProducts(l)
+
+	sm := http.NewServeMux()
+	sm.Handle("/", productsHandler)
+	s := http.Server{
+		Addr:         ":9090",
+		Handler:      sm,
+		ErrorLog:     l,
+		IdleTimeout:  120 * time.Second,
+		WriteTimeout: 1 * time.Second,
+		ReadTimeout:  1 * time.Second,
+	}
 	// start the server
-	http.ListenAndServe(":9090", nil)
+	s.ListenAndServe()
 }
